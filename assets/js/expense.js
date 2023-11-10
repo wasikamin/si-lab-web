d8feb4dd0aefb57abd4518c8de6123290f3337ff
Mohammad Wasik Amin <wasik.amin@shopee.com>
2023-11-10 17:47:34 +0700

    feat: add delete expanse

--- assets/js/expense.js
+++ assets/js/expense.js
@@ -1,6 +1,7 @@
 let currentBalance = 0
 let expense = 0
 let income = 0
+let transactionHistory = []
 
 const formatNumber = (number) => new Intl.NumberFormat('en-DE').format(number)
 
@@ -13,11 +14,38 @@ const submitForm = (e) => {
     for (let inpName of inputName) {
         obj[inpName] = formElements.elements[inpName].value
     }
-    addToCard(obj)
+    transactionHistory.push(obj)
+    refreshTable()
     formElements.reset()
 }
 
-const addToCard = (obj) => {
+const refreshTable = () => {
+    expense = 0
+    income = 0
+    currentBalance = 0
+    deleteTableHistory()
+    if (transactionHistory.length == 0) {
+        return
+    }
+    for (let trans in transactionHistory) {
+        addToCard(transactionHistory[trans], trans)
+    }
+}
+
+const deleteTableHistory = () => {
+    const history = document.getElementById("history-item")
+    while (history.hasChildNodes()) {
+        history.removeChild(history.firstChild)
+    }
+    const balance = document.getElementById("balance")
+    balance.innerText = `Rp 0`
+    const expenseElement = document.getElementById("expense")
+    const incomeElement = document.getElementById("income")
+    incomeElement.innerText = `Rp 0`
+    expenseElement.innerText = `Rp 0`
+}
+
+const addToCard = (obj, id) => {
     if (parseInt(obj.amount) > 0) {
         addToIncome(obj)
     }
@@ -27,7 +55,7 @@ const addToCard = (obj) => {
     else {
         return
     }
-    addToHistory(obj)
+    addToHistory(obj, id)
     addToBalance(obj)
 }
 
@@ -37,12 +65,15 @@ const addToBalance = (obj) => {
     balance.innerText = `Rp ${formatNumber(currentBalance)}`
 }
 
-const addToHistory = (obj) => {
+const addToHistory = (obj, id) => {
     const history = document.getElementById("history-item")
     let newItem = document.createElement("li")
     newItem.className = "d-flex justify-content-between w-100"
-    newItem.innerHTML = `<p>${obj.transaction}</p>
-    <p>${formatNumber(obj.amount)}</p>`
+    newItem.innerHTML = ` <p>${obj.transaction}</p>
+    <div class="d-flex gap-2">
+        <p>${obj.amount}</p>
+        <button class="btn-delete" onclick="deleteRow(${id})"><i class="fa fa-trash text-danger"></i></button>
+    </div>`
     history.append(newItem)
 }
 
@@ -56,5 +87,10 @@ const addToIncome = (obj) => {
     const incomeElement = document.getElementById("income")
     income += parseInt(obj.amount)
     incomeElement.innerText = `Rp ${formatNumber(income)}`
+}
+
+const deleteRow = (id) => {
+    transactionHistory.splice(id, 1)
+    refreshTable()
 }
 
